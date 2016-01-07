b0f6fc150195ca5e06b7e87705121ad2d64a6b4d
JP Hashagen <jayp33e@gmail.com>
2016-01-07 21:06:04 +0100

    Add button that changes TextView

--- app/src/main/java/com/japhdroid/test/espresso/MainActivity.java
+++ app/src/main/java/com/japhdroid/test/espresso/MainActivity.java
@@ -1,7 +1,9 @@
 package com.japhdroid.test.espresso;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ChangeText(View view) {
+        TextView tv = (TextView) findViewById(R.id.text);
+        tv.setText("It works!");
     }
 }
